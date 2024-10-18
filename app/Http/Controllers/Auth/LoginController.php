aef8153d725a2a58882af34e964736e412175231
Ricky Kurniawan <rickykurniawan1117@gmail.com>
2024-10-19 01:40:17 +0700

    Initial commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -35,8 +35,9 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
-        $this->middleware('auth')->only('logout');
+        $this->middleware('guest')->except('login');
+        $this->middleware('auth')->only('login');
+        
     }
     
     protected function redirectTo(Request $request)
